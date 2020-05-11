e387909fce0fb6cf7aed7912bd33c7ad01e36449
Kanahiro <mediterranean1769@gmail.com>
2020-05-12 08:20:37 +0900

    remove unused import

--- geocoder.py
+++ geocoder.py
@@ -1,8 +1,6 @@
 import json
 import urllib
-import ogr, osr
 import requests
-import io
 
 from qgis.core import *
 
@@ -10,23 +8,24 @@ from .configue_dialog import ConfigueDialog
 from .settings_manager import SettingsManager
 from . import utils
 
+
 class MapTilerGeocoder:
     def __init__(self, language='ja'):
         self._language = language
 
     def geocoding(self, searchword, center_lonlat):
-        #apikey validation
+        # apikey validation
         smanager = SettingsManager()
         apikey = smanager.get_setting('apikey')
         if not utils.validate_key(apikey):
             self._openConfigueDialog()
             return
 
-        proximity = '%s,%s'%(str(center_lonlat[0]),str(center_lonlat[1]))
+        proximity = '%s,%s' % (str(center_lonlat[0]), str(center_lonlat[1]))
         params = {
-            'key':apikey,
-            'proximity':proximity,
-            'language':self._language
+            'key': apikey,
+            'proximity': proximity,
+            'language': self._language
         }
         p = urllib.parse.urlencode(params, safe=',')
 
