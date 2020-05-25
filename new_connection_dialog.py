f033413a60685121d3d42d0462789194d195bf9f
kiguchi <mediterranean1769@gmail.com>
2020-05-25 13:18:50 +0900

    connection edit error messagebox added

--- new_connection_dialog.py
+++ new_connection_dialog.py
@@ -5,10 +5,12 @@ import os
 
 from .settings_manager import SettingsManager
 
+
 class NewConnectionDialog(QtWidgets.QDialog):
     def __init__(self, layertype):
         super().__init__()
-        self.ui = uic.loadUi(os.path.join(os.path.dirname(__file__), 'new_connection_dialog_base.ui'), self)
+        self.ui = uic.loadUi(os.path.join(os.path.dirname(
+            __file__), 'new_connection_dialog_base.ui'), self)
         self.ui.button_box.accepted.connect(self._accepted)
         self.ui.button_box.rejected.connect(self._rejected)
 
@@ -18,7 +20,7 @@ class NewConnectionDialog(QtWidgets.QDialog):
         input_name = self.ui.name_txt.text()
         input_url = self.ui.url_txt.text()
 
-        #when inputed URL includes APIKEY
+        # when inputed URL includes APIKEY
         smanager = SettingsManager()
         apikey = smanager.get_setting('apikey')
         if apikey:
@@ -26,19 +28,10 @@ class NewConnectionDialog(QtWidgets.QDialog):
                 apikey_char_count = len(apikey) * -1
                 input_url = input_url[:apikey_char_count]
 
-        if input_name == '':
-            print('layer name is empty, please input name.')
+        if self._has_error():
             return
 
-        if input_url == '':
-            print('layer url is empty, please input name.')
-            return
-            
         maps = smanager.get_setting(self._layertype)
-        if input_name in maps:
-            print(input_name + ' already exist, input other name.')
-            return
-        
         maps[input_name] = input_url
         smanager.store_setting(self._layertype, maps)
 
@@ -47,9 +40,39 @@ class NewConnectionDialog(QtWidgets.QDialog):
     def _rejected(self):
         self.close()
 
+    def _has_error(self):
+        input_name = self.ui.name_txt.text()
+        input_url = self.ui.url_txt.text()
+
+        is_empty_name = False
+        is_empty_url = False
+        is_existing_name = False
+        error_message = ''
+
+        if input_name == '':
+            is_empty_name = True
+            error_message += 'Name is empty, please input.\n'
+
+        if input_url == '':
+            is_empty_url = True
+            error_message += 'Url to map is empty, please input.\n'
+
+        smanager = SettingsManager()
+        maps = smanager.get_setting(self._layertype)
+        if input_name in maps:
+            is_existing_name = True
+            error_message += str('"' + input_name + '"' +
+                                 ' already exists, please input other name.\n')
+
+        if is_empty_name or is_empty_url or is_existing_name:
+            QtWidgets.QMessageBox.warning(None, 'Error', error_message)
+            return True
+
+
 class RasterNewConnectionDialog(NewConnectionDialog):
     def __init__(self):
         super().__init__('rastermaps')
+
 
 class VectorNewConnectionDialog(NewConnectionDialog):
     def __init__(self):
